e00c69fc40f87d5c12b1225f51bf84d105748174
George Codat Tudoran <office@solcreation.ro>
2017-08-18 00:27:43 +0300

    Securing app

--- src/ui.js
+++ src/ui.js
@@ -1,10 +1,12 @@
+import xss from "xss-filters";
 
 let ui = {
 	renderPosts(posts) {
 		//console.log( posts );
 		
 		let elements = posts.map( (post)  => {
-			return articleTemplate;
+			let { title, author } = post;
+			return articleTemplate(title, author);
 		});
 
 		let target = document.querySelector(".container");
@@ -13,13 +15,20 @@ let ui = {
 }
 
 
-let articleTemplate = `
-<article class="post">
-		<h2 class="post-title">
-				Nimeni nu se multumeste cu averea pe care o are, dar fiecare e multumit de inteligenta sa. 
-		</h2>
-		<p class="post-meta">Tolstoi</p>
-		</article>
-`;
+function  articleTemplate(title, author) {
+	let saveTitle = xss.inHTMLData(title);
+	let safeAuthor = xss.inHTMLData(author);
+
+	let template = `<article class="post">
+			<h2 class="post-title">
+					${saveTitle}
+			</h2>
+			<p class="post-meta">
+				${safeAuthor}
+				</p>
+			</article>`;
+
+			return template;
+};
 
 export default ui;
